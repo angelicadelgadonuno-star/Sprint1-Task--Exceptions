afed4bce02f5d2838ed36f421663af71476af993
Angelicadelgadonuno <angelicadelgadonuno@gmail.com>
2026-09-16 20:01:48 +0200

    Refactor: updated class to extend RuntimeException

--- src/main/java/Nivel1/Excepcions/Personalitzades/EmptySaleException.java
+++ src/main/java/Nivel1/Excepcions/Personalitzades/EmptySaleException.java
@@ -1,6 +1,6 @@
 package Nivel1.Excepcions.Personalitzades;
 
-public class EmptySaleException extends Exception {
+public class EmptySaleException extends RuntimeException {
 
     public EmptySaleException() {
         super ("To make a sale you must add products first");
