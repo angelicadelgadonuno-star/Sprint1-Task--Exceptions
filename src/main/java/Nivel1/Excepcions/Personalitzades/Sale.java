48b1d9bb6cd66c9d08675d8107ab65fe001ae161
Angelicadelgadonuno <angelicadelgadonuno@gmail.com>
2026-09-16 15:17:10 +0200

    Created class Sale, added attrubutes and methods

---
+++ src/main/java/Nivel1/Excepcions/Personalitzades/Sale.java
@@ -0,0 +1,29 @@
+package Nivel1.Excepcions.Personalitzades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sale {
+    private List<Product> products = new ArrayList<>();
+    private double totalPrice;
+
+    public double calculateTotal() throws EmptySaleException {
+        if (products.isEmpty()) {
+            throw new EmptySaleException();
+        }
+
+        for (Product p : products) {
+            totalPrice = totalPrice + p.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public void addProduct (Product p) {
+        products.add(p);
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+}
